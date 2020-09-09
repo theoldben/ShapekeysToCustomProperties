7d1190a8b00b7b7a7a4f13a519b1209ad6b32dbd
theoldben <der.blindfisch@gmail.com>
2020-09-09 20:33:43 +0200

    Update ShapeKeysToCustomProperties.py

--- ShapeKeysToCustomProperties.py
+++ ShapeKeysToCustomProperties.py
@@ -12,6 +12,7 @@ active_obj = bpy.context.active_object
 selected_obj = bpy.context.selected_objects
 other_obj =  [obj for obj in bpy.context.selected_objects if obj.type == 'MESH']
 active_bone = bpy.context.active_pose_bone
+active_bone_name = active_bone.name
 key_number = len(other_obj[0].data.shape_keys.key_blocks)
 
 ## Go through Selected Objects and assign the Mesh Object to a variable
@@ -38,13 +39,13 @@ if len(selected_obj) == 2 and active_obj.type == 'ARMATURE' and other_obj[0].typ
             if "_RNA_UI" not in other_obj[0].keys():
                 other_obj[0]['_RNA_UI'] = {}
             obj_rna_ui = other_obj[0].get('_RNA_UI')
+            
             # Store ShapeKey Name
-            varI = other_obj[0].data.shape_keys.key_blocks[i].name
-
+            name_shapekey = other_obj[0].data.shape_keys.key_blocks[i].name
             # Add Property with ShapeKey's name to Active PoseBone and set Value to 0
-            active_bone[varI] = 0.0
+            active_bone[name_shapekey] = 0.0
             # Add Custom Properties to RNA_UI dictionary, fill values
-            obj_rna_ui[varI] = {
+            obj_rna_ui[name_shapekey] = {
                         "default": 0.0,
                         "min":0.0,
                         "max":1.0, 
@@ -53,21 +54,21 @@ if len(selected_obj) == 2 and active_obj.type == 'ARMATURE' and other_obj[0].typ
                         "description":"Shape Key Influence"
                         }
 
-            # Add the Drivers to the ShapeKeys so they can be controlled via Custom Properties                  
+            # Add the Drivers to the ShapeKeys Value Field, so ttheir influence can be controlled via Custom Properties                  
             driver = other_obj[0].data.shape_keys.key_blocks[i].driver_add("value").driver
             # Create new input Variable for driver
             driver_var = driver.variables.new()
             # Set number of Variables in the driver stack, since there is only one, that is 0
             target = driver_var.targets[0]
-            # Set the Target object for the Driver's Variable Input, i.e. the Mesh Object
-            target.id = other_obj[0]
+            # Set the Target object for the Driver's Variable Input, i.e. the Armature Object, where the input sliders are
+            target.id = active_obj
             # Set the drivers Data Path, i.e. the RNA Path to the Property it uses as input
-            target.data_path = "[" +"\""+ varI + "\"" +"]"
+            target.data_path = "pose.bones"+"["+"\""+active_bone.name+"\""+"]"+"[" +"\""+ name_shapekey + "\"" +"]"
             # Set the Expression Field of the Driver, in this case with only the variable name
             driver.expression = driver_var.name
 
     # Set ShapeKey Startup Value
-    other_obj[0][varI] = 0.0
+    other_obj[0][name_shapekey] = 0.0
 
     # property attributes.for UI
     # redraw Properties panel
@@ -80,5 +81,5 @@ if len(selected_obj) == 2 and active_obj.type == 'ARMATURE' and other_obj[0].typ
                 break
 
 else:
-    print("nononono")
+    print("Incorrect Object Selection: Please Select a Mesh Object followed by an Armature Object")
 #     error_selection(self, context)
