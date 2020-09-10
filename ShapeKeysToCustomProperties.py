1117ff5ba292b260769679e76f3fa262cf8a5e9f
theoldben <der.blindfisch@gmail.com>
2020-09-10 02:32:45 +0200

    Added AlphaNumerical Filter, brought Bone-Naming back

--- ShapeKeysToCustomProperties.py
+++ ShapeKeysToCustomProperties.py
@@ -72,16 +72,19 @@ class OBJECT_OT_ShapeKeysToDrivenProps(bpy.types.Operator):
                         
                         # Store ShapeKey Name
                         name_shapekey = other_obj[0].data.shape_keys.key_blocks[i].name
-                        
-                        # Add Property with ShapeKey's name to Active PoseBone and set Value to 0
-                        if "_RNA_UI" not in active_bone.keys():
-                            active_bone['_RNA_UI'] = {}
-                        act_bone_rna_ui = active_bone.get('_RNA_UI')
-                        
+                                                
+                        #Strip current Shapekey Name of all alpha-numerical characters
+                        alphanumeric_filter = filter(str.isalnum, name_shapekey)
+                        alphanumeric_string = "".join(alphanumeric_filter)
+                        clean_name_shapekey = alphanumeric_string
+
                         # If there is already a property with the current shapekey's name, skip
                         if name_shapekey in act_bone_rna_ui:
                             continue
                         
+                        # Add Property with ShapeKey's name to Active PoseBone and set Value to 0
+                        active_bone[name_shapekey] = 0.0
+
                         # Access RNA_UI Dictionary of PoseBone, create if it does not exist
                         if "_RNA_UI" not in active_bone.keys():
                             active_bone['_RNA_UI'] = {}
@@ -108,9 +111,9 @@ class OBJECT_OT_ShapeKeysToDrivenProps(bpy.types.Operator):
                         # Set the Target object for the Driver's Variable Input, i.e. the Armature Object, where the input sliders are
                         target.id = active_obj
                         # Set the drivers Data Path, i.e. the RNA Path to the Property it uses as input
-                        target.data_path = "pose.bones"+"["+"\""+active_bone.name+"\""+"]"+"[" +"\""+ name_shapekey + "\"" +"]"
+                        target.data_path = "pose.bones"+"["+"\""+active_bone.name+"\""+"]"+"[" +"\""+ clean_name_shapekey + "\"" +"]"
                         # Set the Expression Field of the Driver, in this case with only the variable name
-                        driver.expression = driver_var.name
+                        driver.expression = driver_var.name = "var"
 
                 # Set ShapeKey Startup Value
                 other_obj[0][name_shapekey] = 0.0
